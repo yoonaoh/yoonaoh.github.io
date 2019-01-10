8fb3ae7a68e6c003b6e72be37c430e54a5ddd8be
joh21 <j.oh@rice.edu>
2019-01-09 20:39:39 -0600

    created tabs

---
+++ homepage.js
@@ -0,0 +1,23 @@
+// Citation: https://www.w3schools.com/howto/howto_js_tabs.asp
+
+function openTab(evt, tab) {
+  var i, tablink, tabcontent;
+
+  // Hide all tabcontents
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove class "active" from tablinks
+  for (i = 0; i < tablink.length; i++) {
+    tablink[i].className = tablink[i].className.replace(" active", "");
+  }
+
+  // Show the current tab and add "active" class to the button that opened tab
+  document.getElementById(tab).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+$(document).ready(setup);
