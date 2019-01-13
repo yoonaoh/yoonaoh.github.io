298affbe421c0a95c6500913508b3933c66992b5
joh21 <j.oh@rice.edu>
2019-01-13 12:46:50 -0600

    intermediate styling of tabs

--- homepage.js
+++ homepage.js
@@ -10,6 +10,7 @@ function openTab(evt, tab) {
   }
 
   // Remove class "active" from tablinks
+  tablink = document.getElementsByClassName("tablink");
   for (i = 0; i < tablink.length; i++) {
     tablink[i].className = tablink[i].className.replace(" active", "");
   }
@@ -18,6 +19,27 @@ function openTab(evt, tab) {
   document.getElementById(tab).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+// function openCity(evt, cityName) {
+//   // Declare all variables
+//   var i, tabcontent, tablinks;
+//
+//   // Get all elements with class="tabcontent" and hide them
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+//
+//   // Get all elements with class="tablinks" and remove the class "active"
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+//
+//   // Show the current tab, and add an "active" class to the button that opened the tab
+//   document.getElementById(cityName).style.display = "block";
+//   evt.currentTarget.className += " active";
+// }
 
 
 $(document).ready(setup);
