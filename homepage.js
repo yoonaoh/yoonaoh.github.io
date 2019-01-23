92e9b8dd04574327e4113752d00f419ca8395f1e
joh21 <j.oh@rice.edu>
2019-01-22 20:41:39 -0600

    redesigning landing page

--- homepage.js
+++ homepage.js
@@ -1,45 +1,23 @@
 // Citation: https://www.w3schools.com/howto/howto_js_tabs.asp
 
-function openTab(evt, tab) {
-  var i, tablink, tabcontent;
-
-  // Hide all tabcontents
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Remove class "active" from tablinks
-  tablink = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablink.length; i++) {
-    tablink[i].className = tablink[i].className.replace(" active", "");
-  }
-
-  // Show the current tab and add "active" class to the button that opened tab
-  document.getElementById(tab).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-// function openCity(evt, cityName) {
-//   // Declare all variables
-//   var i, tabcontent, tablinks;
+// function openTab(evt, tab) {
+//   var i, tablink, tabcontent;
 //
-//   // Get all elements with class="tabcontent" and hide them
+//   // Hide all tabcontents
 //   tabcontent = document.getElementsByClassName("tabcontent");
 //   for (i = 0; i < tabcontent.length; i++) {
 //     tabcontent[i].style.display = "none";
 //   }
 //
-//   // Get all elements with class="tablinks" and remove the class "active"
-//   tablinks = document.getElementsByClassName("tablinks");
-//   for (i = 0; i < tablinks.length; i++) {
-//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   // Remove class "active" from tablinks
+//   tablink = document.getElementsByClassName("tablink");
+//   for (i = 0; i < tablink.length; i++) {
+//     tablink[i].className = tablink[i].className.replace(" active", "");
 //   }
 //
-//   // Show the current tab, and add an "active" class to the button that opened the tab
-//   document.getElementById(cityName).style.display = "block";
+//   // Show the current tab and add "active" class to the button that opened tab
+//   document.getElementById(tab).style.display = "block";
 //   evt.currentTarget.className += " active";
 // }
-
 
 $(document).ready(setup);
