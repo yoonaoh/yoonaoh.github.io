143c9c8b8da12bd50de379ffbf915cedb75b6ac2
joh21 <j.oh@rice.edu>
2019-01-25 10:46:46 -0600

    drafted portfolio site

--- homepage.js
+++ homepage.js
@@ -1,23 +1,10 @@
-// Citation: https://www.w3schools.com/howto/howto_js_tabs.asp
-
-// function openTab(evt, tab) {
-//   var i, tablink, tabcontent;
-//
-//   // Hide all tabcontents
-//   tabcontent = document.getElementsByClassName("tabcontent");
-//   for (i = 0; i < tabcontent.length; i++) {
-//     tabcontent[i].style.display = "none";
-//   }
-//
-//   // Remove class "active" from tablinks
-//   tablink = document.getElementsByClassName("tablink");
-//   for (i = 0; i < tablink.length; i++) {
-//     tablink[i].className = tablink[i].className.replace(" active", "");
-//   }
-//
-//   // Show the current tab and add "active" class to the button that opened tab
-//   document.getElementById(tab).style.display = "block";
-//   evt.currentTarget.className += " active";
-// }
+function toggleView(idName){
+  var tab = document.getElementById(idName);
+  if (tab.style.display === "none") {
+    tab.style.display = "block";
+  } else {
+    tab.style.display = "none";
+  }
+}
 
 $(document).ready(setup);
