0355559ecceab2aebb213a027393bc5373b0c7b8
joh21 <j.oh@rice.edu>
2019-08-22 22:04:46 -0400

    Need to work on image enlargement

--- index.js
+++ index.js
@@ -21,13 +21,37 @@ function displayOverlay() {
   var projectName = event.target.id;
   var overlay = document.getElementById(projectName + "-overlay");
   overlay.style.display = "flex";
+  // Remove the scroll to top button.
+  var scrollBtn = document.getElementById("topbtn");
+  scrollBtn.style.display = "none";
 }
 
 function closeProject() {
   var parentObj = event.target.parentNode;
   parentObj.style.display = "none";
+  // Remove the scroll to top button.
+  var scrollBtn = document.getElementById("topbtn");
+  scrollBtn.style.display = "block";
 }
 
 function enlargeImage() {
-  
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = event.target;
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  modal.style.display = "block";
+  modalImg.src = img.src;
+  captionText.innerHTML = img.alt;
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
 }
