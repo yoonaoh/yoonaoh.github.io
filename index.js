6981a2178f980a02e8912c058df77f846eb9d614
joh21 <j.oh@rice.edu>
2019-08-22 20:46:47 -0400

    Style changed

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-// function tabView(tabName){
-//
-//   var i, tabs, activeTab;
-//
-//   tabs = document.getElementsByClassName("tabcontent");
-//   activeTab = document.getElementById(tabName);
-//
-//   if (activeTab.style.display === "none") {
-//     activeTab.style.display = "block";
-//     for (i = 0; i < tabs.length; i++) {
-//       if (tabs[i] != activeTab) {
-//         tabs[i].style.display = "none";
-//       }
-//     }
-//   } else {
-//     activeTab.style.display = "none";
-//   }
-// }
-//
-// $(document).ready(setup);
+// Reference: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  var landingHeight = document.getElementById("introduction").style.height;
+  if (document.body.scrollTop > landingHeight || document.documentElement.scrollTop > landingHeight) {
+    document.getElementById("topbtn").style.display = "block";
+  } else {
+    document.getElementById("topbtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function displayOverlay() {
+  var projectName = event.target.id;
+  var overlay = document.getElementById(projectName + "-overlay");
+  overlay.style.display = "flex";
+}
+
+function closeProject() {
+  var parentObj = event.target.parentNode;
+  parentObj.style.display = "none";
+}
+
+function enlargeImage() {
+  
+}
